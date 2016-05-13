9d4e4f24fc498fade46bef6c7bb0c3b29205e094
Murre <353765792@qq.com>
2016-05-13 22:07:17 +0800

    完成遍历省市县三级列表的功能

--- src/com/coolweather/app/db/CoolWeatherOpenHelper.java
+++ src/com/coolweather/app/db/CoolWeatherOpenHelper.java
@@ -21,12 +21,12 @@ public class CoolWeatherOpenHelper extends SQLiteOpenHelper {
 			+"id integer primary key autoincrement,"
 			+"city_name text,"
 			+"city_code text,"
-			+"province_id integer";
+			+"province_id integer)";
 	public static final String CREATE_COUNTY = "create table County("
 			+"id integer primary key autoincrement,"
 			+"county_name text,"
 			+"county_code text,"
-			+"city_id integer";
+			+"city_id integer)";
 	
 	
 	
